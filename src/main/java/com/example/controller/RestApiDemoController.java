858949c74fb88c33c1f9e4b117f58ba06f45bfe9
Korotin Tima <korotin.tima@mail.ru>
2022-07-27 13:43:24 +0300

    n

---
+++ src/main/java/com/example/controller/RestApiDemoController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApiDemoController {
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiDemoController() {
+        coffees.addAll(List.of(
+                new Coffee("Cafe Cereza"),
+                new Coffee("Cafe Ganador"),
+                new Coffee("Cafe Lareno"),
+                new Coffee("Cafe Tres")));
+    }
+
+    @RequestMapping(value = "/coffees", method = RequestMethod.GET)
+    Iterable<Coffee> getCoffees() {
+        return coffees;
+    }
+}
