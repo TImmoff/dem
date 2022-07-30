d9de15483df2b228ca764a9dc4fddc00d83a6716
Korotin Tima <korotin.tima@mail.ru>
2022-07-30 13:41:49 +0300

    put/post..

--- src/main/java/com/example/controller/RestApiDemoController.java
+++ src/main/java/com/example/controller/RestApiDemoController.java
@@ -2,10 +2,17 @@ package com.example.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class RestApiDemoController {
@@ -23,4 +30,35 @@ public class RestApiDemoController {
     Iterable<Coffee> getCoffees() {
         return coffees;
     }
+
+    @GetMapping("?coffees/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id) {
+        for (Coffee c : coffees) {
+            if (c.getId().equals(id)) {
+                return Optional.of(c);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @PostMapping(value = "path")
+    Coffee postMCoffee(@RequestBody Coffee coffee) {
+        // TODO: process POST request
+        coffees.add(coffee);
+        return coffee;
+    }
+
+    @PutMapping("/coffees/{id}")
+    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        // TODO: process PUT request
+        int coffeeIndex = -1;
+        for (Coffee c : coffees) {
+            if (c.getId().equals(id)) {
+                coffeeIndex = coffees.indexOf(c);
+                coffees.set(coffeeIndex, coffee);
+            }
+        }
+        return (coffeeIndex == -1) ? postMCoffee(coffee) : coffee;
+    }
+
 }
